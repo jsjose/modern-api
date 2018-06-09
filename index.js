1065e6a78a6b23762806e2374084221b5b6e931a
José María San José Juárez <josem.sanjose@gmail.com>
2018-06-09 21:20:41 +0200

    API v1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
+const Painting = require('./models/Painting');
 
 const server = hapi.server({
 	port: 4000,
@@ -12,13 +13,37 @@ mongoose.connection.once('open', () => {
 });
 
 const init = async () => {
-        	server.route({
-			method: 'GET',
-			path: '/',
-			handler: function(request, reply) {
-				return '<h1>My modern api</h1>';
-			}
-		});
+        	server.route([
+				{
+					method: 'GET',
+					path: '/api/v1/paintings',
+					config: {
+						description: 'Get all the paintings',
+						tags: ['api', 'v1', 'painting']
+					},
+					handler: (req, reply) => {
+						return Painting.find();
+					}
+				},
+				{
+					method: 'POST',
+					path: '/api/v1/paintings',
+					config: {
+						description: 'Get a specific painting by ID.',
+						tags: ['api', 'v1', 'painting']
+					},
+					handler: (req, reply) => {
+						const { name, url, technique } = req.payload;
+						const painting = new Painting({
+							name,
+							url,
+							technique
+						});
+
+						return painting.save();
+					}
+				}	
+			]);
 
 	await server.start();
 	console.log(`Server running at: ${server.info.uri}`);
